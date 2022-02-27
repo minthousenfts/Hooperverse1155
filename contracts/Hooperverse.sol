bda3648ea9cd330f56f5ea9e4155a9870a2cd91a
Joao Lucas NM <51080629+JoaoNM@users.noreply.github.com>
2022-02-27 23:55:32 +0800

    fixed constructor function issue for ERC1155

--- contracts/Hooperverse.sol
+++ contracts/Hooperverse.sol
@@ -6,8 +6,10 @@ import "hardhat/console.sol";
 // use ERC1155 contract for the token 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
-contract Hooperverse {
-    constructor() {
+contract Hooperverse is ERC1155 {
+
+    // need to fix up the dummy URL 
+    constructor() public ERC1155("https://some.url/api/item.json") {
         console.log("Welcome to the hooperverse!");
     }
 }
