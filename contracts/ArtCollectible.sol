0e06bd24d267a80c15ad3f3b2dee9e00df4cca27
padhyeSohum <73366757+padhyeSohum@users.noreply.github.com>
2022-03-07 23:33:23 -0500

    Update ArtCollectible.sol

--- contracts/ArtCollectible.sol
+++ contracts/ArtCollectible.sol
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+// pragma solidity >=0.4.22 <0.9.0;
+
+pragma solidity ^0.8.1;
 
 import '@openzeppelin/contracts/token/ERC1155/ERC1155.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
@@ -8,10 +10,12 @@ contract ArtCollectible is Ownable, ERC1155 {
     // Base URI
     string private baseURI;
     string public name;
+    event nftMinted(address addressCalled, uint256 tokenId);
+    event nftBatchMinted(address addressCalled, uint256[] tokenIds);
 
     constructor()
         ERC1155(
-            'ipfs://QmfH4te5jjEn87rpS3KiM3D8qciXWXxVZP23gfrbSraBVE/{id}.json' // REPLACE THIS WITH ACTUAL IPFS FROM PINATA ONCE UPLOADS ARE DONE
+            'ipfs://QmfH4te5jjEn87rpS3KiM3D8qciXWXxVZP23gfrbSraBVE/{id}.json'
         )
     {
         setName('Hooperverse NFT Collection');
@@ -29,9 +33,11 @@ contract ArtCollectible is Ownable, ERC1155 {
         public
     {
         _mintBatch(msg.sender, ids, amounts, '');
+        emit nftBatchMinted(msg.sender, ids);
     }
 
     function mint(uint256 id, uint256 amount) public onlyOwner {
         _mint(msg.sender, id, amount, '');
+        emit nftMinted(msg.sender, id);
     }
 }
