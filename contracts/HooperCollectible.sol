3adf746b143ee96cf3c7e6a4f8179280c0a01987
Fazal Mittu <62004613+fazalmittu@users.noreply.github.com>
2022-06-05 10:03:29 -0700

    imports

--- contracts/HooperCollectible.sol
+++ contracts/HooperCollectible.sol
@@ -1,3 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
+
+import '@openzeppelin/contracts/token/ERC1155/ERC1155.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
 
 contract HooperCollectible is Ownable, ERC1155 {
